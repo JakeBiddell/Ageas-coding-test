a80cdb3d0e0bf7ee64758b0ce8775b24acd6f8a9
JakeBiddell <jake.biddell@gmail.com>
2021-09-26 08:41:47 +0100

    added sort reducer

--- src/Slices/User/slice.ts
+++ src/Slices/User/slice.ts
@@ -38,5 +38,40 @@ const slice = createSlice({
       state.loading = false;
       state.error = payload;
     },
+    addPaginatedList(state, { payload }: PayloadAction<string>) {
+      if (!Object.keys(state.paginatedLists).includes(payload)) {
+        state.paginatedLists = {
+          ...state.paginatedLists,
+          [payload]: {
+            ids: Object.keys(state.entityStore).map((id) => +id),
+            sort: undefined,
+            filter: undefined,
+          },
+        };
+      }
+    },
+    sortPaginatedList(
+      state,
+      {
+        payload: { listId, column, direction },
+      }: PayloadAction<{
+        listId: string;
+        column: keyof User;
+        direction: SortDirection;
+      }>
+    ) {
+      state.paginatedLists[listId].sort = { column, direction };
+      const allEntities = Object.values(state.entityStore);
+      const filter = state.paginatedLists[listId].filter;
+      const filteredEntities = !filter
+        ? allEntities
+        : allEntities.filter((e) => e[filter.column] === filter.value);
+      const sortedEntities = filteredEntities.sort(
+        direction === "asc"
+          ? (a, b) => a[column].toString().localeCompare(b[column].toString())
+          : (a, b) => b[column].toString().localeCompare(a[column].toString())
+      );
+      state.paginatedLists[listId].ids = sortedEntities.map((e) => e.id);
+    },
   },
 });
